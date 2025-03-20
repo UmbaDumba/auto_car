008f4d4821c605ff2bf3cca92f4d312f31e1d556
UmbaDumba <dkdi3104@gmail.com>
2025-03-20 09:48:18 +0900

    init_commit, create project

---
+++ AUTO_CAR/AUTO_CAR/main.c
@@ -0,0 +1,18 @@
+/*
+ * AUTO_CAR.c
+ *
+ * Created: 2025-03-20 오전 9:47:33
+ * Author : microsoft
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
